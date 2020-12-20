201f8fe4197d8d796d09bf7158156a0858b1c46b
Zachary Zhu <Acturos@users.noreply.github.com>
2020-12-20 09:52:50 +0800

    save current work

--- apps/cifar10/alexnet.py
+++ apps/cifar10/alexnet.py
@@ -5,6 +5,37 @@ import torch.nn as nn
 from Mnn_Core.mnn_pytorch import *
 from torch.utils.tensorboard import SummaryWriter
 
+"""
+AlexNet(
+  (features): Sequential(
+    (0): Conv2d(3, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2))
+    (1): ReLU(inplace=True)
+    (2): MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False)
+    (3): Conv2d(64, 192, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2))
+    (4): ReLU(inplace=True)
+    (5): MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False)
+    (6): Conv2d(192, 384, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+    (7): ReLU(inplace=True)
+    (8): Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+    (9): ReLU(inplace=True)
+    (10): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+    (11): ReLU(inplace=True)
+    (12): MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False)
+  )
+  (avgpool): AdaptiveAvgPool2d(output_size=(6, 6))
+  (classifier): Sequential(
+    (0): Dropout(p=0.5, inplace=False)
+    (1): Linear(in_features=9216, out_features=4096, bias=True)
+    (2): ReLU(inplace=True)
+    (3): Dropout(p=0.5, inplace=False)
+    (4): Linear(in_features=4096, out_features=4096, bias=True)
+    (5): ReLU(inplace=True)
+    (6): Linear(in_features=4096, out_features=1000, bias=True)
+  )
+)
+
+"""
+
 
 class AlexNet(nn.Module):
     def __init__(self, num_classes: int = 1000) -> None:
@@ -90,8 +121,14 @@ class Mnn_Alex_Net(nn.Module):
         return x
 
 
-model = torch.hub.load('pytorch/vision:v0.6.0', 'alexnet', pretrained=True)
+data_path = 'D:\Data_repos\Imagenet2012'
+model = torchvision.models.alexnet(pretrained=True)
 model.eval()
-
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 for name in model.state_dict():
-    print(model.state_dict()[name].shape)
+    print(name, type(model.state_dict()[name]))
